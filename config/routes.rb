1f56bc3d184dd78fe8cf38dc8717e14cf710d6da
Ahyun Seo <studio.lastborn@gmail.com>
2016-02-10 13:23:18 +0000

    routes setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
   root 'pages#home'
+  
   get '/home', to:'pages#home'
+ # get '/journeys', to: 'journeys#index' 
+ # get '/journeys/new', to: 'journeys#new', as: 'new_journey'
+ # post '/journeys', to: 'journeys#create'
+ # get '/journeys/:id/edit', to:'journeys#edit', as: 'edit_journey'
+ # patch '/journeys/:id', to:'journeys#update'
+ # get '/journeys/:id', to:'journeys#show', as:'journey'
+ # delete '/journeys/:id', to:'journeys#destroy'
+
+  resources :journeys
 end
