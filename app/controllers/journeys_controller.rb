77788b0d1001254c41d9babf1ffbb5fa8363432b
Ahyun Seo <studio.lastborn@gmail.com>
2016-02-11 10:03:56 +0000

    add partials, msg, errors

--- app/controllers/journeys_controller.rb
+++ app/controllers/journeys_controller.rb
@@ -5,4 +5,35 @@ class JourneysController < ApplicationController
     def show
       @journey=Journey.find(params[:id])
     end
+    def new
+      @journey=Journey.new
+    end
+    def create
+      @journey=Journey.new(journey_params)
+      @journey.traveler = Traveler.find(1)
+        if @journey.save
+          flash[:success] = "Your journey was posted successfully!"
+          redirect_to journeys_path
+        else
+          render :new
+        end
+    end
+    def edit
+      @journey=Journey.find(params[:id])
+    end
+    def update
+      @journey=Journey.find(params[:id])
+      if @journey.update(journey_params)
+        flash[:success] = "Your journey was edited successfully!"
+        redirect_to journey_path(@journey)
+      else
+        render :edit
+      end
+    end
+    
+    
+    private
+      def journey_params
+        params.require(:journey).permit(:title, :location, :description)
+      end
 end
