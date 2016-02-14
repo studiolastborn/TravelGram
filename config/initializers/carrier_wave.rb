663e112953491b9833d326f3687ed3a65e3af70a
Ahyun Seo <studio.lastborn@gmail.com>
2016-02-14 15:16:13 +0000

    aws error

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,12 +1,12 @@
 if Rails.env.production?
   CarrierWave.configure do |config|
     config.aws_credentials = {
-      :provider => 'AWS',
       :aws_access_key_id => ENV['S3_ACCESS_KEY'],
       :aws_secret_access_key => ENV['S3_SECRET_KEY'],
       :region => 'Seoul'
       
     }
+      config.storage    = :aws
       config.aws_bucket = ENV['S3_BUCKET']
   end
 end
