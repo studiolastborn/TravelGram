2c79041a61b44ff9a83e4626ecd5e279afe151ff
Ahyun Seo <studio.lastborn@gmail.com>
2016-02-14 15:32:16 +0000

    region

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -4,7 +4,7 @@ if Rails.env.production?
       :provider => 'AWS',
       :aws_access_key_id => ENV['S3_ACCESS_KEY'],
       :aws_secret_access_key => ENV['S3_SECRET_KEY'],
-      :region => 'Seoul'
+      :region => 'ap-northeast-2'
       
     }
       config.aws_bucket = ENV['S3_BUCKET']
