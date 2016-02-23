d8b41d719519d9a1851daff17d8cc159fb99c44a
Ahyun Seo <studio.lastborn@gmail.com>
2016-02-23 07:27:57 +0000

    Again

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -6,6 +6,8 @@ class PictureUploader < CarrierWave::Uploader::Base
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
   process resize_to_limit: [600,600]
+  
+
 
   # Choose what kind of storage to use for this uploader:
   if Rails.env.production?
@@ -16,6 +18,10 @@ class PictureUploader < CarrierWave::Uploader::Base
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
   #   # For Rails 3.1+ asset pipeline compatibility:
